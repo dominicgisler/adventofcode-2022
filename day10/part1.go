a7966188083907ea6d35b6e00624a1729ad6cc1e
Dominic Gisler <domgisler@gmail.com>
2022-12-12 10:24:46 +0100

    day 10

--- day10/part1.go
+++ day10/part1.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func (d *Day10) part1() {
-	fmt.Println("TODO")
+
+	x := 1
+	cycle := 0
+
+	var stack []string
+	for _, line := range d.InputLines {
+		if line == "noop" {
+			stack = append(stack, line)
+		} else if strings.HasPrefix(line, "addx") {
+			stack = append(stack, "noop", "noop", line)
+		}
+	}
+
+	strength := 0
+	checkCycle := 20
+	for _, cmd := range stack {
+		if cmd == "noop" {
+			cycle++
+		}
+
+		if cycle == checkCycle {
+			checkCycle += 40
+			strength += cycle * x
+		}
+
+		if strings.HasPrefix(cmd, "addx") {
+			parts := strings.Split(cmd, " ")
+			num, _ := strconv.Atoi(parts[1])
+			x += num
+		}
+	}
+
+	fmt.Println("total strength:", strength) // 17020
 }
--- day10/part2.go
+++ day10/part2.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func (d *Day10) part2() {
-	fmt.Println("TODO")
+
+	x := 1
+	cycle := 0
+	crt := ""
+
+	var stack []string
+	for _, line := range d.InputLines {
+		if line == "noop" {
+			stack = append(stack, line)
+		} else if strings.HasPrefix(line, "addx") {
+			stack = append(stack, "noop", "noop", line)
+		}
+	}
+
+	for _, cmd := range stack {
+		if cmd == "noop" {
+			cycle++
+
+			if x >= cycle%40-2 && x <= cycle%40 {
+				crt += "#"
+			} else {
+				crt += "."
+			}
+
+			if cycle%40 == 0 {
+				crt += "\n"
+			}
+		}
+
+		if strings.HasPrefix(cmd, "addx") {
+			parts := strings.Split(cmd, " ")
+			num, _ := strconv.Atoi(parts[1])
+			x += num
+		}
+	}
+
+	fmt.Println(crt) // RLEZFLGE
 }
